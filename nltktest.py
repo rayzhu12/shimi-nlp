77427b415d3b9cdbc5954c2465c5ceacbed96394
Raymond <rayzhu12@gmail.com>
2019-02-07 12:20:45 -0500

    added porterstemmer
    porterstemmer kinda sucks

--- nltktest.py
+++ nltktest.py
@@ -1,16 +1,26 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
 import nltk 
 nltk.download('stopwords') 
 nltk.download('punkt')
 
 example_sentence = "this is an example showing off stop word filtration." 
-stop_words = set(stopwords.words("english"))
+stop_words = set(stopwords.words("english")) # stop words are unnecessary for meaning
 
 # print(stop_words)
-filtered_sentence = []
-words = word_tokenize(example_sentence)
+words = word_tokenize(example_sentence) #splits all the words 
+filtered_sentence = [w for w in words if not w in stop_words]
+
+# for w in words:
+#     if w not in stop_words:
+#         filtered_sentence.append(w)
+# print(filtered_sentence)
+
+ps = PorterStemmer()
+example_words = ["python", "pythoner", "pythoning", 'pythoned', 'pythonly']
+# for w in example_words:
+#     print(ps.stem(w))
 for w in words:
-    if w not in stop_words:
-        filtered_sentence.append(w)
-print(filtered_sentence)
+    print(ps.stem(w))
